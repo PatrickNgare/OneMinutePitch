962566c5de6e93d0391e3ad17e2e1f659d7a314f
patrickNgare <patrickngare@gmail.com>
2018-02-14 14:50:50 +0300

    added config file

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+
+class Config:
+   
+    SECRET_KEY = 'PatrickNgare'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://patel:12345@localhost/pate'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+   
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://patel:12345@localhost/pate'
+
+
+class ProdConfig(Config):
+    
+    pass
+   
+
+    
+class DevConfig(Config):
+   
+
+    
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
